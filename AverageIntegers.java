b78f40456f48b989ce65b95c99e2f8db3aad631d
“jpayers86” <jpayers86@gmail.com>
2017-10-18 11:37:27 -0400

    I added the averageintegers source code

---
+++ AverageIntegers.java
@@ -0,0 +1,13 @@
+import acm.program.*;
+
+public class AverageIntegers extends ConsoleProgram{
+	public void run(){
+		int x1 = 5;
+		int x2 = 4;
+		int x3 = 6;
+		int x4 = 8;
+		double average = ((x1+x2+x3+x4)/(double)(4));
+		println(average);
+	}
+
+}
